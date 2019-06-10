36874264f4848f1cb8ae29a5771520a4d0c05c97
kayryu <lukayryu@gmail.com>
2019-06-10 16:45:35 +0800

    Update bootnodes

--- run.sh
+++ run.sh
@@ -17,6 +17,6 @@ PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 EXE_PATH="$PWD/target/debug/ladder"
 BASE_PATH="$PWD/$NODE_NAME"
 
-RUST_LOG='info' $EXE_PATH --chain=ladder --base-path=$BASE_PATH --key=$KEY --name=$NODE_NAME --bootnodes /ip4/192.168.1.109/tcp/30333/p2p/QmYzQGPSnXSXY8PiMBeeeh6e9kHA8j6F7aJdjwWzNxnGUi --port $PORT --validator --telemetry-url ws://telemetry.polkadot.io:1024 > $LOG_FILE 2>&1 & 
+RUST_LOG='info' $EXE_PATH --chain=ladder --base-path=$BASE_PATH --key=$KEY --name=$NODE_NAME --bootnodes /ip4/119.3.110.159/tcp/30333/p2p/QmUHCcUVntmijQJhMJeF6H3ApYracxfriX8Gjha5eK8jn6 --port $PORT --validator --telemetry-url ws://telemetry.polkadot.io:1024 > $LOG_FILE 2>&1 & 
 
 echo "Node run with $NODE_NAME, Log in $LOG_FILE file"
